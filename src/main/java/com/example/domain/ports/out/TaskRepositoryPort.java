ac5f558a8641de110da42bd89ebc5ad50bee9a0c
Tu Nombre <andresyalca@gmail.com>
2025-01-06 17:44:42 -0500

    tercero

--- src/main/java/com/example/domain/ports/out/TaskRepositoryPort.java
+++ src/main/java/com/example/domain/ports/out/TaskRepositoryPort.java
@@ -2,6 +2,8 @@ package com.example.domain.ports.out;
 import java.util.List;
 import java.util.Optional;
 
+
+// vamos ha hacerlo
 import com.example.domain.models.Task;
 public interface ExternalServicePot {
   Task save(Task task)
