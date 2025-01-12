e362b8616eea838100fa121a22dbcb24b8b6e775
Tu Nombre <andresyalca@gmail.com>
2025-01-11 21:01:52 -0500

    agregue punto y coma linea 9

--- src/main/java/com/example/domain/ports/out/TaskRepositoryPort.java
+++ src/main/java/com/example/domain/ports/out/TaskRepositoryPort.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 // vamos ha hacerlo
 import com.example.domain.models.Task;
 public interface ExternalServicePot {
-  Task save(Task task)
+  Task save(Task task);
   Optional<Task> findById(Long id);
   List<Task> findAll();
   Optional<Task> updateTask(Task task);
